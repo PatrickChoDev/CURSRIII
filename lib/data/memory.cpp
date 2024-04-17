cfaf9c47cafb613de7c69942432fdfe676a07c32
Thanapat <devpatrick.cho@gmail.com>
2024-04-17 17:17:58 +0700

    feat: time and memory module

---
+++ lib/data/memory.cpp
@@ -0,0 +1,75 @@
+#include <memory.hpp>
+
+void CURSRFilesystem::log(char *message)
+{
+#ifdef MEMORY_LOG_ENABLED
+  Serial.println(message);
+#endif
+}
+
+void CURSRFilesystem::setup()
+{
+  log("Setting up filesystem...");
+  if (SD.begin(SD_CARD_SS_PIN))
+  {
+    this->memoryAvailable = true;
+    log("Filesystem setup complete.");
+  }
+  else
+  {
+    this->memoryAvailable = false;
+    log("Filesystem setup failed.");
+  }
+  loadFlightStage();
+}
+
+FlightStage CURSRFilesystem::getFlightStage()
+{
+  return flightStage;
+}
+
+void CURSRFilesystem::loadFlightStage()
+{
+  log("Loading flight stage...");
+  flightStage = SD.exists("/flight_stage") ? (FlightStage)SD.open("/flight_stage").read() : PRELAUNCH;
+  log("Flight stage loaded.");
+}
+
+void CURSRFilesystem::saveFlightStage()
+{
+  log("Saving flight stage...");
+  File flightStageFile = SD.open("/flight_stage", FILE_WRITE);
+  flightStageFile.write(flightStage);
+  flightStageFile.close();
+  log("Flight stage saved.");
+}
+
+void CURSRFilesystem::setFlightStage(FlightStage flightStage)
+{
+  this->flightStage = flightStage;
+}
+
+void CURSRFilesystem::writeLog(char *message)
+{
+  log(message);
+
+  systemLogFile.print(time(nullptr)); // TODO Receive timestamp from GPS
+  systemLogFile.print(": ");
+  systemLogFile.println(message);
+}
+
+void CURSRFilesystem::logData(char *data)
+{
+  log(data);
+  // Get current timestamp
+  time_t currentTime = time(nullptr);
+
+  // Convert timestamp to string
+  char timestamp[20];
+  strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", localtime(&currentTime));
+
+  // Write timestamp to flight log file
+  flightLogFile.print(timestamp);
+  flightLogFile.print(", ");
+  flightLogFile.println(data);
+}
---
+++ lib/data/memory.hpp
@@ -0,0 +1,48 @@
+#ifndef MEMORY_LOG_ENABLED
+#define MEMORY_LOG_ENABLED 1
+#endif
+
+#ifndef SD_CARD_SS_PIN
+#define SD_CARD_SS_PIN 34
+#endif
+
+#ifdef ARDUINO_H
+#include <Arduino.h>
+#endif
+
+#ifndef SD_H
+#define SD_H
+#include <SD.h>
+#endif
+
+#ifndef MEMORY_HPP
+#define MEMORY_HPP
+
+enum FlightStage
+{
+  PRELAUNCH,
+  ASCENT,
+  DESCENT,
+  POSTFLIGHT
+};
+
+class CURSRFilesystem
+{
+private:
+  bool memoryAvailable = false;
+  void setup();
+  void log(char *message);
+  FlightStage flightStage = PRELAUNCH;
+  File systemLogFile = SD.open("system_log", FILE_WRITE);
+  File flightLogFile = SD.open("flight_log", FILE_WRITE);
+
+public:
+  FlightStage getFlightStage();
+  void loadFlightStage();
+  void saveFlightStage();
+  void setFlightStage(FlightStage flightStage);
+  void writeLog(char *message);
+  void logData(char *data);
+}
+
+#endif
